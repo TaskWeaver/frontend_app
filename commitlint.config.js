fff5182f2a744e16e598b8b0624afcf1f49f8235
fridayCCC <fridayccccc@gmail.com>
2023-12-29 16:50:13 +0900

    chore:change commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,18 +6,7 @@ module.exports = {
     "type-enum": [
       2,
       "always",
-      [
-        "build",
-        "chore",
-        "ci",
-        "docs",
-        "feat",
-        "feature",
-        "fix",
-        "refactor",
-        "style",
-        "test",
-      ],
+      ["chore", "docs", "feat", "fix", "refactor", "style", "test"],
     ],
     "type-empty": [2, "never"],
     "type-case": [2, "always", "lower-case"],
@@ -36,24 +25,4 @@ module.exports = {
     "footer-leading-blank": [1, "always"],
     "footer-max-line-length": [2, "always", 100],
   },
-  plugins: [
-    {
-      rules: {
-        "develop-rule": ({ subject }) => {
-          const commitFolders = ["[data]", "[ui]"];
-          return [
-            commitFolders.some(
-              (folder) =>
-                subject?.startsWith(folder) !== subject?.endsWith(folder)
-            ),
-            `\n${commitFolders.map((folder) => `${folder}\n`).join("")}
-위 네 가지 중 한 가지는 반드시 콜론(:) 뒤에 포함되어야 합니다.
-[name] 뒤에 메시지 입력은 필수입니다.
-
-ex) feat: [frontend] 마우스 더블클릭 후 스타일 변경`,
-          ];
-        },
-      },
-    },
-  ],
 };
