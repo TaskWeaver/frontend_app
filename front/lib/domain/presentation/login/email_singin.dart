6d527ed863c0deb58ccb66bdfdf4ae28726b0918
brownglasses <lhj1583@naver.com>
2023-12-31 23:28:16 +0900

    feat/email_signinScreen

---
+++ front/lib/domain/presentation/login/email_singin.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+import 'package:front/domain/presentation/login/component/RoundedElvatedButton.dart';
+import 'package:front/domain/presentation/login/component/checkbox_text_row.dart';
+import 'package:front/domain/presentation/login/component/customTextField.dart';
+
+class EmailSignInScreen extends StatefulWidget {
+  const EmailSignInScreen({super.key});
+
+  @override
+  State<EmailSignInScreen> createState() => _EmailSignInScreenState();
+}
+
+class _EmailSignInScreenState extends State<EmailSignInScreen> {
+  GlobalKey<FormState> formKey = GlobalKey();
+  String? id;
+  String? nickName;
+  String? confirmPassword;
+  String? password;
+  String? name;
+  String? phoneNumber;
+  bool personalInfoAgreed = false;
+  bool termsAgreed = false;
+  bool allAgreed = false;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: SafeArea(
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: Column(
+                  children: [
+                    const Text("회원가입"),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 100, // 원의 너비
+                        height: 100, // 원의 높이
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle, // 동그라미 모양
+                          color: Colors.grey[300], // 회색
+                        ),
+                        child: const Center(child: Text("이미지")),
+                      ),
+                    ),
+                    CustomTextField(
+                      hintText: "아이디",
+                      onSaved: (String? val) {
+                        id = val;
+                      },
+                      validator: idValidator,
+                    ),
+                    CustomTextField(
+                      hintText: "닉네임",
+                      onSaved: (String? val) {
+                        nickName = val;
+                      },
+                      validator: nickNameValidator,
+                    ),
+                    CustomTextField(
+                      hintText: "비밀번호",
+                      onChanged: (String? val) {
+                        confirmPassword = val;
+                      },
+                      onSaved: (String? val) {
+                        password = val;
+                      },
+                      validator: passwordValidator,
+                    ),
+                    CustomTextField(
+                      hintText: "비밀번호확인",
+                      onSaved: (String? val) {
+                        confirmPassword = val;
+                      },
+                      validator: confirmPasswordValidator,
+                    ),
+                    CustomTextField(
+                      hintText: "이름",
+                      onSaved: (String? val) {
+                        name = val;
+                      },
+                      validator: nameValidator,
+                    ),
+                    CustomTextField(
+                      hintText: "전화번호",
+                      onSaved: (String? val) {
+                        phoneNumber = val;
+                      },
+                      validator: phoneNumberValidator,
+                    ),
+                    CheckboxTextRow(
+                      text: '모두 동의',
+                      value: allAgreed,
+                      onChanged: (value) {
+                        setState(() {
+                          allAgreed = value!;
+                          personalInfoAgreed = value;
+                          termsAgreed = value;
+                        });
+                      },
+                    ),
+                    Expanded(
+                      flex: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2.0,
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: Column(children: [
+                            CheckboxTextRow(
+                              value: personalInfoAgreed,
+                              onChanged: (value) {
+                                setState(() {
+                                  personalInfoAgreed = value!;
+                                  updateAllAgreed();
+                                });
+                              },
+                              text: '개인 정보 약관 동의',
+                            ),
+                            CheckboxTextRow(
+                              value: termsAgreed,
+                              onChanged: (value) {
+                                setState(() {
+                                  termsAgreed = value!;
+                                  updateAllAgreed();
+                                });
+                              },
+                              text: '이용 약관 동의',
+                            ),
+                          ]),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    RoundedElevatedButton(
+                      text: "회원가입",
+                      onPressed: onSingInPressed,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void updateAllAgreed() {
+    setState(() {
+      allAgreed = personalInfoAgreed && termsAgreed;
+    });
+  }
+
+  String? idValidator(String? val) {
+    if (val == null || val.isEmpty) {
+      return 'ID를 입력해주세요';
+    }
+    return null;
+  }
+
+  String? nickNameValidator(String? val) {
+    if (val == null || val.isEmpty) {
+      return '닉네임을 입력해주세요';
+    }
+    return null;
+  }
+
+  String? passwordValidator(String? val) {
+    if (val == null || val.isEmpty) {
+      return '비밀번호를 입력해주세요';
+    }
+    return null;
+  }
+
+  String? confirmPasswordValidator(String? val) {
+    if (val == null || val.isEmpty) {
+      return '비밀번호를 입력해주세요';
+    }
+    if (val != confirmPassword) {
+      return '비밀번호가 일치하지 않습니다';
+    }
+    return null;
+  }
+
+  String? nameValidator(String? val) {
+    if (val == null || val.isEmpty) {
+      return '이름을 입력해주세요';
+    }
+    return null;
+  }
+
+  String? phoneNumberValidator(String? val) {
+    if (val == null || val.isEmpty) {
+      return '전화번호를 입력해주세요';
+    }
+    return null;
+  }
+
+  onSingInPressed() {
+    if (formKey.currentState!.validate() && allAgreed == true) {
+      formKey.currentState!.save();
+      print(id);
+      print(nickName);
+      print(confirmPassword);
+      print(password);
+      print(name);
+      print(phoneNumber);
+    }
+  }
+}
