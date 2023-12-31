382d7aff8dfafa19f7cb3f772ef5c7b058616f6b
brownglasses <lhj1583@naver.com>
2023-12-31 23:29:22 +0900

    feat/routesUpdate

---
+++ front/lib/config/routes.dart
@@ -0,0 +1,18 @@
+import 'package:front/domain/presentation/home/home.dart';
+import 'package:front/domain/presentation/login/emailSingIn.dart';
+import 'package:front/domain/presentation/login/login.dart';
+import 'package:front/domain/presentation/login/signIn.dart';
+
+class RouteName {
+  static const home = '/';
+  static const login = '/login';
+  static const signIn = '/signIn';
+  static const emailSignIn = '/emailSignIn';
+}
+
+var nameRoutes = {
+  RouteName.home: (context) => const HomeScreen(),
+  RouteName.login: (context) => const LoginScreen(),
+  RouteName.signIn: (context) => const SignInScreen(),
+  RouteName.emailSignIn: (context) => const EmailSignInScreen(),
+};
