fdd9c138eaea4011ddc609fceb51c14bdc752e41
brownglasses <lhj1583@naver.com>
2024-01-01 19:59:58 +0900

    feat: rounded_elvatedbutton 컴포넌트 코드 작성
    둥근 모서리 버튼이 앱에 자주 나타나 컴포넌트로 작성하였습니다.
    String text, VoidCallback onPressed 을 받습니다.
    
    resolves: #1

---
+++ front/lib/domain/presentation/login/component/rounded_elvatedbutton.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class RoundedElevatedButton extends StatelessWidget {
+  const RoundedElevatedButton(
+      {super.key, required this.text, required this.onPressed});
+  final String text;
+  final VoidCallback onPressed;
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle = const TextStyle(color: Colors.black, fontSize: 12);
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          shadowColor: Colors.transparent,
+          backgroundColor: Colors.grey[300],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0), // 원하는 둥근 모서리 반지름 값 지정
+          ),
+          padding: const EdgeInsets.symmetric(
+              horizontal: 16.0, vertical: 12.0), // 원하는 패딩 값 지정
+        ),
+        child: Text(
+          text,
+          style: textStyle,
+        ),
+      ),
+    );
+  }
+}
