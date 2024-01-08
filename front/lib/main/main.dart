e7a53e35159a860ab7858a6e167ceaaded0b74ae
fridayCCC <fridayccccc@gmail.com>
2024-01-08 22:00:39 +0900

    chore: split dev mode

--- front/lib/main/main.dart
+++ front/lib/main/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:front/app/config/routes.dart';
-
-void main() async {
-  runApp(const MyApp());
-}
+import 'package:front/main/app_config.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Call AppConfig.of(context) anywhere to obtain the
+    // environment specific configuration
+    var config = AppConfig.of(context)!;
+
     return MaterialApp(
+      title: config.appName,
       debugShowCheckedModeBanner: false,
       routes: nameRoutes,
     );
