28808498677daf8205a7c0163d46f370d2d98586
brownglasses <lhj1583@naver.com>
2024-01-17 18:55:58 +0900

    feat: add Assets, Spider

---
+++ front/lib/resources/images.dart
@@ -0,0 +1,9 @@
+part of 'resources.dart';
+
+class Images {
+  Images._();
+
+  static const String iconBook = 'assets/images/icon_book.png';
+  static const String iconCheck = 'assets/images/icon_check.png';
+  static const String iconPencil = 'assets/images/icon_pencil.png';
+}
