15129cb0ef275892c3c3c3083c33a8a4a53821a7
Ejun Park <jjoonleo@gmail.com>
2024-02-26 17:35:11 +0900

    fix: remove team_id field and create managerId to ProjectRequestModel

--- front/lib/core/project/data/models/project_create.dart
+++ front/lib/core/project/data/models/project_create.dart
@@ -1,16 +1,16 @@
 import 'package:equatable/equatable.dart';
 
-class ProjectCreateModel extends Equatable {
-  const ProjectCreateModel({
-    required this.team_id,
+class ProjectRequestModel extends Equatable {
+  const ProjectRequestModel({
+    required this.managerId,
     required this.name,
     required this.description,
   });
-  
-  final String team_id;
+
+  final int managerId;
   final String name;
   final String description;
 
   @override
-  List<Object?> get props => [ team_id, name, description ];
+  List<Object?> get props => [managerId, name, description];
 }
