902c19abe435d3ca62e600bba1c6018853c937e2
Ejun Park <jjoonleo@gmail.com>
2024-02-26 15:53:32 +0900

    feat: create project remoteDataSource getProjectsByTeamId method

--- front/lib/core/project/data/data_sources/remote_data_source.dart
+++ front/lib/core/project/data/data_sources/remote_data_source.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:front/core/project/data/models/project.dart';
 import 'package:front/core/project/data/models/project_create.dart';
+import 'package:front/core/utils/exception.dart';
 
 abstract class ProjectRemoteDataSource {
   Future<List<ProjectModel>> getProjectsByTeamId(int teamId);
@@ -11,31 +12,40 @@ abstract class ProjectRemoteDataSource {
 }
 
 class ProjectRemoteDataSourceImpl implements ProjectRemoteDataSource {
-    ProjectRemoteDataSourceImpl({required this.dio});
-    final Dio dio;
+  ProjectRemoteDataSourceImpl({required this.dio});
+  final Dio dio;
 
   @override
   Future<List<ProjectModel>> getProjectsByTeamId(int teamId) async {
+    var response = await dio.post(
+      '/v1/projects/$teamId',
+    );
+
+    if (response.statusCode == 200) {
+      return response.data['result'].map<ProjectModel>((data) => ProjectModel.fromJson(data)).toList();
+    } else {
+      throw ServerException();
+    }
   }
-  
+
   @override
   Future<ProjectModel> getProjectById(int projectId) {
     // TODO: implement getProjectById
     throw UnimplementedError();
   }
-  
+
   @override
   Future<ProjectModel> createProject(ProjectCreateModel project) {
     // TODO: implement createProject
     throw UnimplementedError();
   }
-  
+
   @override
   Future<String> deleteProjectById(String projectId) {
     // TODO: implement deleteProject
     throw UnimplementedError();
   }
-  
+
   @override
   Future<ProjectModel> updateProject(ProjectModel project) {
     // TODO: implement updateProject
