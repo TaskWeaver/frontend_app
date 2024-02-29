4338bd3059055fa0867c0c0f7ed315b709545830
brownglasses <lhj1583@naver.com>
2024-02-29 14:38:39 +0900

    fix: fix  datasource proiver

--- front/lib/core/user/data_sources/rivorpod.dart
+++ front/lib/core/user/data_sources/rivorpod.dart
@@ -1,13 +1,26 @@
 import 'package:front/core/config/providers/dio.dart';
+import 'package:front/core/user/data_sources/local_data_source.dart';
 import 'package:front/core/user/data_sources/remote_data_source.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'rivorpod.g.dart';
 
 @riverpod
-AuthRemoteDataSource authRemoteDataSource(
-  AuthRemoteDataSourceRef ref,
+UserRemoteDataSource userRemoteDataSource(
+  UserRemoteDataSourceRef ref,
 ) {
   var dio = ref.watch(dioProvider);
-  return AuthRemoteDataSourceImpl(dio: dio);
+  return UserRemoteDataSourceImpl(dio: dio);
+}
+
+@Riverpod(keepAlive: true)
+Future<SharedPreferences> sharedPref(SharedPrefRef ref) async {
+  return await SharedPreferences.getInstance();
+}
+
+@riverpod
+UserLocalDataSource userLocalDataSource(UserLocalDataSourceRef ref) {
+  var sharedPrefs = ref.watch(sharedPrefProvider).requireValue;
+  return UserLocalDataSource(sharedPreferences: sharedPrefs);
 }
