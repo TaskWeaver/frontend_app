824c1f5d23e7b83f265d673bb40cc7b45a453c5c
brownglasses <lhj1583@naver.com>
2024-02-29 14:30:18 +0900

    fix: fix UserModel 기존 유저 모델을 좀 더 직렬적으로 받아올 수 있게 수정하였습니다.

--- front/lib/core/user/models/user.dart
+++ front/lib/core/user/models/user.dart
@@ -1,42 +1,52 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'user.freezed.dart';
 part 'user.g.dart';
 
-///공통 필드
-abstract class UserBase {
-  int? get id; // 회원가입 요청에서는 제외될 수 있음
-  String get email;
-  String? get nickname;
-  String? get imageUrl;
-}
-
 ///User의 로그인 및 마이페이지 조회 정보를 받기위해 사용되는 모델
-@freezed
-class UserModel with _$UserModel implements UserBase {
-  const UserModel._(); // private constructor
-  const factory UserModel({
-    required int id,
-    required String email,
-    String? nickname, // 이 부분을 수정
-    String? imageUrl,
-    String? accessToken,
-    String? refreshToken,
-    required String type,
-  }) = _UserModel;
+
+@JsonSerializable()
+class UserModel {
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.nickname, // 이 부분을 수정
+    this.imageUrl,
+    required this.type,
+  });
 
   factory UserModel.fromJson(Map<String, dynamic> json) =>
       _$UserModelFromJson(json);
+  final int id;
+  final String email, nickname, type;
+  final String? imageUrl;
+
+  Map<String, dynamic> toJson() => _$UserModelToJson(this);
+}
+
+@JsonSerializable()
+class LoginResponse {
+  final int id;
+  final String email, type, accessToken, refreshToken;
+
+  LoginResponse(
+      {required this.id,
+      required this.email,
+      required this.type,
+      required this.accessToken,
+      required this.refreshToken});
+
+  factory LoginResponse.fromJson(Map<String, dynamic> json) =>
+      _$LoginResponseFromJson(json);
+  Map<String, dynamic> toJson() => _$LoginResponseToJson(this);
 }
 
 ///회원가입 요청에서 사용되는 모델
 @freezed
-class UserRegistrationRequest
-    with _$UserRegistrationRequest
-    implements UserBase {
+class UserRegistrationRequest with _$UserRegistrationRequest {
   const UserRegistrationRequest._(); // private constructor
   const factory UserRegistrationRequest({
-    int? id, // Optional, 회원가입 요청에서는 id가 없을 수 있음
     required String email,
     required String nickname,
     String? imageUrl,
@@ -49,15 +59,12 @@ class UserRegistrationRequest
 
 ///회원가입 시 서버로부터 받는 응답
 @freezed
-class UserRegistrationResponse
-    with _$UserRegistrationResponse
-    implements UserBase {
+class UserRegistrationResponse with _$UserRegistrationResponse {
   const UserRegistrationResponse._(); // private constructor
   const factory UserRegistrationResponse({
-    int? id, // Optional, 회원가입 요청에서는 id가 없을 수 있음
+    required int id, // Optional, 회원가입 요청에서는 id가 없을 수 있음
     required String email,
-    required String nickname,
-    String? imageUrl, // UserRegistration 특유의 필드
+    required String nickname, // UserRegistration 특유의 필드
   }) = _UserRegistrationResponse;
 
   factory UserRegistrationResponse.fromJson(Map<String, dynamic> json) =>
