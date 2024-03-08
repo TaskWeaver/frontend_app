fa9e4db8e2e92afaa6aaf2b0487299f494715237
ejun <jjoonleo@gmail.com>
2024-03-08 16:50:53 +0900

    feat: firebase cloud message setup

--- front/lib/main/main_dev.dart
+++ front/lib/main/main_dev.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:front/firebase_options.dart';
 import 'package:front/main/app_config.dart';
+import 'package:front/main/firebase.dart';
 import 'package:front/main/main.dart';
 
-void main() {
+void main() async {
+  setUpFirebaseCloudMessage();
+
   var configuredApp = const AppConfig(
     appName: 'Build flavors DEV',
     flavorName: 'development',
--- front/lib/main/main_production.dart
+++ front/lib/main/main_production.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:front/firebase_options.dart';
 import 'package:front/main/app_config.dart';
 import 'package:front/main/main.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   var configuredApp = const AppConfig(
     appName: 'Build flavors',
     flavorName: 'production',
