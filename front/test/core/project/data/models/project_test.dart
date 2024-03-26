9d60b504c05745b4cbdfb85780f1d830ea4dac32
brownglasses <lhj1583@naver.com>
2024-03-26 10:51:58 +0900

    "revert: change class name in test"
    This reverts commit 72b80c41129d91568f87ab8b43d4d0ddbbef9661.

--- front/test/core/project/data/models/project_test.dart
+++ front/test/core/project/data/models/project_test.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:front/core/const/enum.dart';
 import 'package:front/core/project/data/models/project.dart';
-import 'package:front/core/project/domain/entities/project_entity.dart';
+import 'package:front/core/project/domain/entities/project.dart';
 
 import '../../../../helpers/json_reader.dart';
 
@@ -15,7 +15,7 @@ void main() {
     managerId: 2,
     projectState: 'BEFORE',
   );
-  var testProject = ProjectEntity(
+  var testProject = Project(
     projectId: 1,
     name: 'name1',
     description: 'description1',
